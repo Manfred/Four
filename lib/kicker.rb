ab54a2637b903137f420c1a86bfdc52a26ec3dda
Manfred Stienstra <manfred@fngtps.com>
2013-12-02 21:06:20 +0100

    Implement reporting and watch pwd by default.

--- lib/kicker.rb
+++ lib/kicker.rb
@@ -1,2 +1,22 @@
+require 'pty'
+require 'tidings'
+
 class Kicker
+  class << self
+    attr_accessor :formatter
+  end
+
+  self.formatter = Proc.new do |time, message|
+    time.strftime('%H:%M:%S.') + time.usec.to_s[0,2] + ' | ' + message
+  end
+
+  def self.report(message)
+    $stdout.puts(formatter.call(Time.now, message))
+  end
+
+  def self.run
+    Tidings.watch(Dir.pwd) do |file_or_path, flags|
+      Kicker.report("Event: #{file_or_path}: #{flags.inspect}")
+    end
+  end
 end
